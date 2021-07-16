bc9ae0ffa61e2aff541ef79870d6ddebd4e07f1e
Mike-OxHuge <mikelitoris34@icloud.com>
2021-07-17 00:38:45 +0200

    adds link to repo in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,9 @@
   <v-container fluid class="text-center">
     <p>P.S: This app HAS NOTHING to do with Strive school staff.</p>
     <p>It was made by one of its students.</p>
+    <a href="https://github.com/Mike-OxHuge/strivenator" target="_blank"
+      >Source code</a
+    >
   </v-container>
 </template>
 
